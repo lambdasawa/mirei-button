77eb5d1667c11661833e3f49103257565fa6c8fc
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-04-13 02:21:35 +0900

    :sparkles: Use ECS for trimmer

--- infra/bin/mirei-button.ts
+++ infra/bin/mirei-button.ts
@@ -2,8 +2,21 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { MireiButtonStack } from "../lib/mirei-button-stack";
+import { MireiButtonTrimmerStack } from "../lib/mirei-button-trimmer-stack";
 import { getStage } from "../lib/stage";
 
 const app = new cdk.App();
 const stage = getStage(app);
-new MireiButtonStack(app, `${stage.stackPrefix}MireiButtonStack`);
+const props = {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+};
+new MireiButtonStack(app, `${stage.stackPrefix}MireiButtonStack`, props);
+new MireiButtonTrimmerStack(
+  app,
+  `${stage.stackPrefix}MireiButtonTrimmerStack`,
+  props
+);
+app.synth();
